a7a120b9c51bfefb2a689ef3042bd60888625394
Matti Luukkainen <mluukkai@iki.fi>
2014-11-18 13:43:24 +0200

    data scaffold js show synched with coffee

--- generators/data-module/templates/scripts/_ShowController.js
+++ generators/data-module/templates/scripts/_ShowController.js
@@ -3,14 +3,26 @@ angular
   .controller("ShowController", function ($scope, <%= _.capitalize(resourceName) %>, supersonic) {
     $scope.<%= resourceName %> = null;
     $scope.showSpinner = true;
+    $scope.dataId = undefined;
 
-    supersonic.ui.views.current.whenVisible( function () {
-      <%= _.capitalize(resourceName) %>.find(steroids.view.params.id).then( function (<%= resourceName %>) {
+    var _refreshViewData = function () {
+      <%= _.capitalize(resourceName) %>.find($scope.dataId).then( function (<%= resourceName %>) {
         $scope.$apply( function () {
           $scope.<%= resourceName %> = <%= resourceName %>;
           $scope.showSpinner = false;
         });
       });
+    }
+
+    supersonic.ui.views.current.whenVisible( function () {
+      if ( $scope.dataId ) {
+        _refreshViewData();
+      }
+    });
+
+    supersonic.ui.views.current.params.onValue( function (values) {
+      $scope.dataId = values.id;
+      _refreshViewData();
     });
 
     $scope.remove = function (id) {
