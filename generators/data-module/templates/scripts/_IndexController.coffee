7e052c84ed566acea459991535f92c9ea2c2c909
Eevert Saukkokoski <eevert.saukkokoski@appgyver.com>
2014-12-02 16:14:28 +0200

    do not chain whenVisible and whenChanged

--- generators/data-module/templates/scripts/_IndexController.coffee
+++ generators/data-module/templates/scripts/_IndexController.coffee
@@ -4,10 +4,8 @@ angular
     $scope.<%= resourceName %>s = null
     $scope.showSpinner = true
 
-    supersonic.ui.views.current.whenVisible ->
-      <%= _.capitalize(resourceName) %>.all().whenChanged (<%= resourceName %>s) ->
-        $scope.$apply ->
-          $scope.<%= resourceName %>s = <%= resourceName %>s
-          $scope.showSpinner = false
-
+    <%= _.capitalize(resourceName) %>.all().whenChanged (<%= resourceName %>s) ->
+      $scope.$apply ->
+        $scope.<%= resourceName %>s = <%= resourceName %>s
+        $scope.showSpinner = false
   )
--- generators/data-module/templates/scripts/_IndexController.js
+++ generators/data-module/templates/scripts/_IndexController.js
@@ -4,13 +4,10 @@ angular
     $scope.<%= resourceName %>s = null;
     $scope.showSpinner = true;
 
-    supersonic.ui.views.current.whenVisible( function () {
-      <%= _.capitalize(resourceName) %>.all().whenChanged( function (<%= resourceName %>s) {
-          $scope.$apply( function () {
-            $scope.<%= resourceName %>s = <%= resourceName %>s;
-            $scope.showSpinner = false;
-          });
-      });
+    <%= _.capitalize(resourceName) %>.all().whenChanged( function (<%= resourceName %>s) {
+        $scope.$apply( function () {
+          $scope.<%= resourceName %>s = <%= resourceName %>s;
+          $scope.showSpinner = false;
+        });
     });
-
   });
