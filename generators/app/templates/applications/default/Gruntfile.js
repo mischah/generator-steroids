51c733c87c29abfd753b7de6e74804858ddd1932
Eevert Saukkokoski <eevert.saukkokoski@appgyver.com>
2015-01-28 17:06:22 +0200

    use cloud resource setup from grunt-steroids with a bit of manual twiddling

--- generators/app/templates/applications/default/Gruntfile.js
+++ generators/app/templates/applications/default/Gruntfile.js
@@ -10,7 +10,17 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks("grunt-steroids");
+  grunt.loadTasks(__dirname + "/node_modules/grunt-steroids/tasks/fresh");
 
-  grunt.registerTask("default", ["steroids-make", "steroids-compile-sass"]);
+  grunt.registerTask('copy-cloud-raml-to-config', function() {
+    grunt.file.copy((__dirname + '/www/cloud.raml'), (__dirname + '/config/cloud-resources.raml'))
+  });
+  
+  grunt.registerTask("default", [
+    "steroids-make",
+    "steroids-compile-sass",
+    "copy-cloud-raml-to-config",
+    "steroids-setup-cloud-resources"
+  ]);
 
 };
